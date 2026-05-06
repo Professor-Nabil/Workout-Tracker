f889f98426353056506ffb2faaa48865944aa70c
Nabil Tester <tester@example.com>
2026-05-06 11:05:48 +0100

    feat: database models

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-console.log("smoke");
+/* ==========================================================================================
+ * Database Models
+ * */
+
+class User {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+class Category {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+class Exercise {
+  constructor(id, name, description, category) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.category = category;
+  }
+}
+class Exercise_Plan {
+  constructor(
+    id,
+    exercise,
+    numberOfRepetitions,
+    sets,
+    weights,
+    exerciseTime,
+    comment,
+  ) {
+    this.id = id;
+    this.exercise = exercise;
+    this.numberOfRepetitions = numberOfRepetitions;
+    this.sets = sets;
+    this.weights = weights;
+    this.exerciseTime = exerciseTime;
+    this.comment = comment;
+  }
+}
+class Plan_Status {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+class Workout_Plans {
+  constructor(id, user, exercisePlan, planStatus) {
+    this.id = id;
+    this.user = user;
+    this.exercisePlan = exercisePlan;
+    this.planStatus = planStatus;
+  }
+}
+class Workouts_Progress {
+  constructor(id, user, numberOfCompletedExercises) {
+    this.id = id;
+    this.user = user;
+    this.numberOfCompletedExercises = numberOfCompletedExercises;
+  }
+}
+
