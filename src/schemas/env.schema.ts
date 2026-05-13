16ea42ad849cb6af48337d2003aa9d1e975025a9
Nabil Tester <tester@example.com>
2026-05-13 18:46:38 +0100

    chore: zod env schema

--- src/schemas/env.schema.ts
+++ src/schemas/env.schema.ts
@@ -1,3 +1,18 @@
-/*
+/* ./src/schemas/env.schema.ts
  * Zod Schemas
  */
+import "dotenv/config";
+import z from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.error(`Invalid Environment Variables: ${_env.error.format()}`);
+  process.exit(1);
+}
+
+export const env = _env.data;
