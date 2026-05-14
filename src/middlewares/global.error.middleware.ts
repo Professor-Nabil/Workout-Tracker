cd5f737ce3c629c01856c7435ec08b7ea9b586a3
Nabil Tester <tester@example.com>
2026-05-14 10:36:37 +0100

    feat: add global error middleware

--- src/middlewares/global.error.middleware.ts
+++ src/middlewares/global.error.middleware.ts
@@ -1,4 +1,21 @@
-/*
- * Global Error Handler (The safety net)
- * - Return the standard `JSON` error response
- */
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../lib/app.error.js";
+
+export const globalErrorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (err.code === "P2002") {
+    err = new AppError("Email already exists", 409);
+  }
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
