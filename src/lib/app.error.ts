6938d4a22fcfcb145e24c8e2f44eb1bd5a878b2a
Nabil Tester <tester@example.com>
2026-05-14 10:35:29 +0100

    feat: app error conflect error classes

--- src/lib/app.error.ts
+++ src/lib/app.error.ts
@@ -1,3 +1,22 @@
-/*
- * Custom Errors (AppError, ConflictError, etc..)
- */
+// ./src/lib/app.error.ts
+export class AppError extends Error {
+  public readonly statusCode: number;
+  public readonly status: string;
+  public readonly isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class ConflectError extends AppError {
+  constructor(message: string) {
+    super(message, 409);
+  }
+}
