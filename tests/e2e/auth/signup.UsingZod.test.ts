4ba93b2b65ddb0c8a3b6b4cb28abd0c7987acd83
Nabil Tester <tester@example.com>
2026-05-16 17:18:44 +0100

    test: update the signup test happy path

--- tests/e2e/auth/signup.UsingZod.test.ts
+++ tests/e2e/auth/signup.UsingZod.test.ts
@@ -1,27 +1,7 @@
 import request from "supertest";
 import app from "../../../src/app.js";
 import { faker } from "@faker-js/faker";
-import z from "zod";
-
-const responseSchema = z.object({
-  status: z.literal(201),
-  body: z
-    .object({
-      message: z.string().min(5),
-      data: z
-        .object({
-          user: z
-            .object({
-              id: z.string().uuid(),
-              email: z.string().email(),
-            })
-            .strict(),
-          token: z.string().jwt(),
-        })
-        .strict(),
-    })
-    .strict(),
-});
+import { sginupResponseSchema } from "../../../src/schemas/response/auth/signup.schema.js";
 
 const user = {
   email: faker.internet.email(),
@@ -32,7 +12,7 @@ describe("API POST /auth/signup", () => {
   it("Happy path", async () => {
     const { status, body } = await request(app).post("/auth/signup").send(user);
 
-    const result = responseSchema.safeParse({ status, body });
+    const result = sginupResponseSchema.safeParse({ status, body });
 
     if (!result.success) {
       const message = result.error.issues.map((e) => {
