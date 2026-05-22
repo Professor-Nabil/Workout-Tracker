b4bfd47ab51b5818ed7ecd746d56ca08f9ae2cbe
Nabil Tester <tester@example.com>
2026-05-22 18:35:29 +0100

    feat: schema validation for 'update complex plan'

--- src/modules/plan/plan.schema.ts
+++ src/modules/plan/plan.schema.ts
@@ -18,3 +18,21 @@ export const planSchema = z.object({
 });
 
 export type PlanSchema = z.infer<typeof planSchema>;
+
+export const planUpdateSchema = z.object({
+  planId: z.uuid(),
+  title: z.string().min(3).max(100),
+  planExercises: z.array(
+    z.union([
+      z.object({
+        exerciseId: z.uuid(),
+        weight: z.coerce.number().optional(),
+        sets: z.coerce.number().optional(),
+        reps: z.coerce.number().optional(),
+        period: z.coerce.number().optional(),
+      }),
+    ]),
+  ),
+});
+
+export type PlanUpdateSchema = z.infer<typeof planUpdateSchema>;
