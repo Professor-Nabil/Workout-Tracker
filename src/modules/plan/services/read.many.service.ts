599431304573eed8278a83f5e857abe0906c9a30
Nabil Tester <tester@example.com>
2026-05-26 18:39:35 +0100

    refactor(service): read.many.plans to inclute exercise and category and tool

--- src/modules/plan/services/read.many.service.ts
+++ src/modules/plan/services/read.many.service.ts
@@ -3,7 +3,11 @@ import db from "../../../lib/db.js";
 export const readManyService = async (userId: string) => {
   const result = await db.plan.findMany({
     where: { userId },
-    include: { planExercise: true },
+    include: {
+      planExercise: {
+        include: { exercise: { include: { category: true, tool: true } } },
+      },
+    },
   });
 
   return result;
